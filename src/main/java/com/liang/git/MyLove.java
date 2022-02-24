4b1de254c386cd5fa05955660f863055c2f61b0a
hongrongLiang <3120005026@mail2.gdut.edu.cn>
2022-02-24 20:10:30 +0800

    分支第一次提交

--- src/main/java/com/liang/git/MyLove.java
+++ src/main/java/com/liang/git/MyLove.java
@@ -4,5 +4,6 @@ public class MyLove {
     public static void main(String[] args) {
         System.out.println("我喜欢你张小微");
         System.out.println("我爱你张小微");
+        System.out.println("我爱你傻瓜张小微哈哈哈");
     }
 }
