e8c2a3ce46f4f144ee5bc83ccbb442e156a907e9
梁先生 <81372437+CTYD-liang@users.noreply.github.com>
2022-02-24 23:01:09 +0800

    pull commit

--- src/main/java/com/liang/git/MyLove.java
+++ src/main/java/com/liang/git/MyLove.java
@@ -2,8 +2,9 @@ package com.liang.git;
 
 public class MyLove {
     public static void main(String[] args) {
-        System.out.println("我喜欢你张小微");
-        System.out.println("我爱你张小微");
-        System.out.println("我爱你傻瓜张小微哈哈哈");
+        System.out.println("我喜欢你张小姐");
+        System.out.println("我爱你张小姐");
+        System.out.println("我爱你傻瓜张小姐哈哈哈");
+        System.out.println("拉取下来，我爱你小傻瓜张小姐哈哈哈");
     }
 }
